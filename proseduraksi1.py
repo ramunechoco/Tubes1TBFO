9683a7da9c6b23335a83f71f244863096857ae3f
Ramunechoco <48040337+ramunechoco@users.noreply.github.com>
2019-09-29 12:57:03 +0700

    Update proseduraksi1.py

--- proseduraksi1.py
+++ proseduraksi1.py
@@ -1,25 +1,51 @@
-def TidurSiang():
-	energy = energy + 10
+def Checker(num,var):
+	if (num < 0) or (num > 15):
+		print('Aksi tidak valid')
+	else:
+		var = num
 
-def TidurMalam():
-	energy = energy + 15
+def TidurSiang(energy):
+	ret = energy + 10
+	Checker(ret,energy)
 
-def MakanHamburger():
-	energy = energy + 5
+def TidurMalam(energy):
+	ret = energy + 15
+	Checker(ret,energy)
 
-def MakanPizza():
-	energy = energy + 10
+def MakanHamburger(energy):
+	ret = energy + 5
+	Checker(ret,energy)
 
-def MakanSteak():
-	energy = energy + 15
+def MakanPizza(energy):
+	ret = energy + 10
+	Checker(ret,energy)
 
-def MinumAir():
-	hygiene = hygiene - 5
+def MakanSteak(energy):
+	ret = energy + 15
+	Checker(ret,energy)
 
-def MinumKopi():
-	hygiene = hygiene - 10
-	energy = energy + 5
+def MinumAir(energy):
+	ret = hygiene - 5
+	Checker(ret,energy)
 
-def MinumJus():
-	hygiene = hygiene - 5
-	energy = energy + 10
+def MinumKopi(energy,hygiene):
+	ret1 = hygiene - 10
+	ret2 = energy + 5
+	Checker(ret,energy)
+	Checker(ret,hygiene)
+
+def MinumJus(energy,hygiene):
+	ret1 = hygiene - 5
+	ret2 = energy + 10	
+	Checker(ret,energy)
+	Checker(ret,hygiene)
+	
+def BuangAirKecil(hygiene):
+	ret = hygiene + 5
+	Checker(ret,hygiene)
+	
+def BuangAirBesar(hygiene,energy):
+	ret1 = hygiene + 10
+	ret2 = energy - 5
+	Checker(ret,energy)
+	Checker(ret,hygiene)
