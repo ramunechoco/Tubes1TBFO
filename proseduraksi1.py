e3274ba5a245530532452a61cf2d2c58e8ea75d4
Ramunechoco <48040337+ramunechoco@users.noreply.github.com>
2019-09-29 12:41:36 +0700

    Add files via upload

---
+++ proseduraksi1.py
@@ -0,0 +1,25 @@
+def TidurSiang():
+	energy = energy + 10
+
+def TidurMalam():
+	energy = energy + 15
+
+def MakanHamburger():
+	energy = energy + 5
+
+def MakanPizza():
+	energy = energy + 10
+
+def MakanSteak():
+	energy = energy + 15
+
+def MinumAir():
+	hygiene = hygiene - 5
+
+def MinumKopi():
+	hygiene = hygiene - 10
+	energy = energy + 5
+
+def MinumJus():
+	hygiene = hygiene - 5
+	energy = energy + 10
